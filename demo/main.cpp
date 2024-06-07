8972a0f64bbf07b457f1c3ab15e60b2fda967f9c
Miaan <nghiabn2005@gmail.com>
2024-06-07 23:19:00 +0700

    Add glfw lib

--- demo/main.cpp
+++ demo/main.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
 
 #include "mia.h"
+#include <glfw3.h>
 
 int main()
 {
     std::cout << "Hello World";
+
+    GLFWwindow* window = glfwCreateWindow(640, 480, "My Title", NULL, NULL);
 
     mia::Init();
 }
