acd26846a27619c31d8f8766b9064fd6dc1621ef
Krakra123 <nghiabn2005@gmail.com>
2024-11-30 11:02:10 +0700

    Use CTest

---
+++ test/entity-test.cpp
@@ -0,0 +1,12 @@
+#include <gtest/gtest.h>
+#include "../include/mia.hpp"
+
+TEST(EntityTest, AddFunction) {
+    
+}
+
+int main(int argc, char** argv) 
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
